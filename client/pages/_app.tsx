d114d56984c01247bb1d4ab5b4ce04bd1370fd4c
beenchangseo <beenchangseo@gmail.com>
2022-07-09 23:09:38 +0900

    [프론트엔드] URL Path 분기 설정 삭제

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,29 +2,16 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from "../components/Layout";
 import Head from "next/head";
-import {NextRouter, useRouter} from "next/router";
-import {useEffect, useState} from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router: NextRouter = useRouter();
-  const [isLogin, setIsLogin] = useState(false);
-  useEffect(()=>{
-    if ((router.asPath).includes('auth')){
-      setIsLogin(true);
-    }
-  },[router])
+
   return <>
     <Head>
       <title>코테 연습장</title>
     </Head>
-    {
-      isLogin ?
-        <Component {...pageProps} />
-          :
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-    }
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   </>
 }
 
