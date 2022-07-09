cd83c88c1e5905c9741b0d6f73f3b2dbb42ad0ef
beenchangseo <beenchangseo@gmail.com>
2022-07-09 23:05:15 +0900

    [프론트엔드] 로그인 페이지 구현 (OAuth 버튼 미구현)

--- client/pages/auth/login.tsx
+++ client/pages/auth/login.tsx
@@ -1,14 +1,128 @@
 import {NextPage} from "next";
+import Link from "next/link";
 
 const Login: NextPage = () => {
 
 
     return <>
-        <div>
-            <form>
-                <input type="submit" value="로그인" />
+        <div className={'signIn-container'}>
+            <form className={'signIn-form'}>
+                <div className={'signIn-form-contents'}>
+                    <div className={'logo'}>
+                        <img src={'/logo.png'} alt={"logo"}/>
+                    </div>
+                    <div className={'title'}>
+                        <h3>Codevelop</h3>
+                    </div>
+                    <div className={'Input'}>
+
+                        <input type={'text'} placeholder={'Email'}/>
+                        <label></label>
+                    </div>
+                    <div className={'Input'}>
+                        <input type={'password'} placeholder={'password'}/>
+                        <label></label>
+                    </div>
+                    <div className={'button-section'}>
+                        <ul>
+                            <li>
+                                <button>Sign In</button>
+                            </li>
+                            <li>
+                                <Link href={'/auth/signup'}>
+                                    <button>Sign Up</button>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </form>
         </div>
+        <style jsx>{`
+          .signIn-container{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 100vw;
+            height: 100vh;
+          }
+          .signIn-form{
+            width: 260px;
+            border-radius: 8px;
+            background-color: #284b63cc;
+          }
+          .signIn-form-contents{
+            
+          }
+          .logo{
+            display: flex;
+            justify-content: center;
+            margin-top: 30px;
+          }
+          .title{
+            display: flex;
+            color: white;
+            justify-content: center;
+          }
+          .title > h3{
+            margin-top: 0;
+            margin-bottom: 20px;
+          }
+          .Input{
+            display: flex;
+            justify-content: center;
+          }
+          .Input > input{
+            border: 0;
+            margin-left: 12px;
+            background: transparent;
+            color: #ffffffb3;
+            border-bottom: 1px solid #58a4b0;
+            width: 65%;
+            font-size: 14px;
+          }
+          label{
+            color: white;
+            font-size: 14px;
+            margin-top: 17px;
+          }
+          .Input input[type=text] ~ label::before {
+            content: "";
+          }
+          .Input input[type=password] ~ label::before {
+            content: "";
+          }
+          .button-section{
+            margin-top: 15px;
+          }
+          .button-section>ul{
+            padding: 0;
+            margin-left: 25px;
+            margin-right: 25px;
+            display: flex;
+            justify-content: space-between;
+            list-style: none;
+          }
+          .button-section>ul:before{
+              display: inline-block;
+              width: 1px;
+              content: '';
+          }
+          .button-section>ul:after{
+              display: inline-block;
+              width: 1px;
+              content: '';
+          }
+          .button-section>ul>li>button{
+            width: 65px;
+            padding: 7px;
+            border: 0;
+            border-radius: 4px;
+            background-color: #58a4b0;
+            color: white;
+            cursor: pointer;
+          }
+        `}</style>
     </>
 }
 
