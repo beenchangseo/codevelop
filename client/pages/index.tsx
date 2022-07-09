8b24f902b36f3e4c95e90aa0372dc3ddbf2990ce
beenchangseo <beenchangseo@gmail.com>
2022-07-09 23:06:11 +0900

    [프론트엔드] 네비게이션 구조 변경

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
 import ChallengeNav from "../components/learn/ChallengeNav";
 import ChallengeContents from "../components/learn/ChallengeContents";
+import TopNav from "../components/TopNav";
 
 const Home: NextPage = () => {
   return <>
+    <TopNav/>
     <div className={"learn-contents"}>
       <ChallengeNav/>
       <ChallengeContents/>
