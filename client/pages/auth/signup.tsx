173f18be8dac6b2f2b0e8a109cfa2b456b370af1
beenchangseo <beenchangseo@gmail.com>
2022-07-11 23:59:55 +0900

    [프론트엔드] 회원가입 구현

--- client/pages/auth/signup.tsx
+++ client/pages/auth/signup.tsx
@@ -1,11 +1,18 @@
 import {NextPage} from "next";
 import React, {useState} from "react";
 import axios from "axios";
+import Link from "next/link";
+import TopNav from "../../components/TopNav";
 
 const Signup: NextPage = () => {
     const [inputs, setInputs] = useState({
-        nickname: '',
+        user_name: '',
         email: ''
+    });
+    const [message, setMessage] = useState({
+        isSuccess: false,
+        isError: false,
+        message: ''
     })
     const inputChangHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
         const {value, name} = event.target;
@@ -16,36 +23,57 @@ const Signup: NextPage = () => {
     }
     const submitFormHandler = async (event:React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        console.log('why is run ?')
-        // const {nickname, email} = inputs;
-        // const response = await fetch('/api/user/signup', {
-        //     method: 'POST',
-        //     headers: {
-        //         "Content-Type": "application/json",
-        //     },
-        //     body: JSON.stringify({nickname, email})
-        // });
-
+        await axios.post('/api/user/signup', {
+            email: inputs.email,
+            user_name: inputs.user_name
+        }).then(response => {
+            console.log("회원 가입 완료",response)
+            if (response.status === 201){
+                setMessage({
+                    isSuccess: true,
+                    isError: false,
+                    message: '계정 생성이 완료되었습니다'
+                });
+            }
+        }).catch((error)=>{
+            if (error.response.status === 409){
+                setMessage({
+                    isSuccess: false,
+                    isError: true,
+                    message: error.response.data.message
+                })
+            }
+        });
     }
-    const emailVerifyHandler = async () => {
-        const response = await axios.post('/api/user/email-verify', inputs.email);
-        console.log(response)
-
-    }
-
     return <>
         <div className={'signUp-container'}>
             <h3>WELCOME CODEVELOPE</h3>
             <div className={'signUp-box'}>
-                <label>Email</label>
-                <div>
-                    <input type={'text'} name={'email'} onChange={inputChangHandler} value={inputs.email} placeholder={'Email'}/>
-                    <button onClick={emailVerifyHandler}>Email Check</button>
-                </div>
+                {message.isError ?
+                    <div className={'message-box error'}>
+                        <a>{message.message}</a>
+                    </div>
+                    : null
+                }
+                {message.isSuccess ?
+                    <div className={'message-box success'}>
+                        <a>{message.message}</a>
+                        <a className={'loginBtn'} href={'/auth/login'}>Login →</a>
+                    </div>
+                    : null
+                }
                 <form className={'signUp-form'} onSubmit={submitFormHandler}>
-                    <label>Name : </label>
-                    <input type={'text'} name={'nickname'} onChange={inputChangHandler} value={inputs.nickname} placeholder={'Name'} disabled={true}/>
-                    <input type={"submit"} value={'SignUp'} disabled={true}/>
+                    <label>Name</label>
+                    <div>
+                        <input type={'text'} name={'user_name'} onChange={inputChangHandler} value={inputs.user_name} placeholder={'Name'}/>
+                    </div>
+                    <br/>
+                    <label>Email</label>
+                    <div>
+                        <input type={'text'} name={'email'} onChange={inputChangHandler} value={inputs.email} placeholder={'Email'}/>
+                    </div>
+
+                    <button style={{width:'100%', marginTop:'30px'}}>Create Account</button>
                 </form>
             </div>
         </div>
@@ -63,12 +91,47 @@ const Signup: NextPage = () => {
           .signUp-box{
             padding: 20px;
             border: 3px solid white;
-            border-radius: 10px;
+            border-radius: 7px;
           }
           .signUp-form{
-            //display: flex;
-            //flex-flow: column;
             color: white;
+          }
+          .signUp-form > div > input{
+            padding: 4px 4px 4px 15px;
+            background-color: transparent;
+            border-radius: 7px;
+            border: 1px solid #d1d5db;
+            width: 250px;
+            height: 20px;
+            color: white;
+          }
+          .message-box{
+            padding: 12px;
+            margin-bottom: 20px;
+            border-radius: 7px;
+            font-size: 10px;
+          }
+          .error{
+            background-color: #fef2f2;
+            color: red;
+          }
+          .success{
+            background-color: #f0fdf4;
+            color: #15803d
+          }
+          .loginBtn{
+            float: right;
+            text-decoration: none;
+            color: #15803d;
+            font-weight: bold;
+            font-size: 12px;
+            margin: 0;
+            padding: 0;
+          }
+          button{
+            border-radius: 7px;
+            padding: 10px;
+            border: 0;
           }
         `}</style>
     </>
