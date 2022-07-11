29da9bd552336bd78556f1507f4f1d14c61374bc
beenchangseo <beenchangseo@gmail.com>
2022-07-12 00:02:33 +0900

    [백엔드] 로그인 유저 이메일 DTO

--- server/src/user/dto/email-verify.dto.ts
+++ server/src/user/dto/email-verify.dto.ts
@@ -1,11 +1,8 @@
-import {IsNotEmpty, IsString} from 'class-validator';
+import {IsEmail, IsNotEmpty, IsString} from 'class-validator';
 
 export class EmailVerifyDto {
     @IsString()
     @IsNotEmpty()
-    nickname: string;
-
-    @IsString()
-    @IsNotEmpty()
+    @IsEmail()
     email: string;
 }
