6bba60beebdb62eff293570b0c69aca189969d19
beenchangseo <beenchangseo@gmail.com>
2022-07-12 00:01:07 +0900

    [백엔드] 회원가입 API 구현

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,19 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, ValidationPipe} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import {EmailVerifyDto} from "./dto/email-verify.dto";
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post('signup')
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
-  }
 
-  @Get()
-  findAll() {
-    return this.userService.findAll();
+  @Post('signup')
+  @UsePipes(ValidationPipe)
+  signUp(@Body() createUserDto: CreateUserDto): Promise<void>{
+    return this.userService.signUp(createUserDto);
+  }
+  @Post('email-verify')
+  @UsePipes(ValidationPipe)
+  emailVerify(@Body() emailVerifyDto: EmailVerifyDto){
+    return this.userService.emailVerify(emailVerifyDto);
   }
 }
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,8 +1,9 @@
-import {Inject, Injectable} from '@nestjs/common';
+import {ConflictException, Inject, Injectable} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import {User} from "./entities/user.entity";
 import {Repository} from "typeorm";
+import {EmailVerifyDto} from "./dto/email-verify.dto";
 
 @Injectable()
 export class UserService {
@@ -12,12 +13,48 @@ export class UserService {
   ) {
   }
 
-  async create(createUserDto: CreateUserDto): Promise<void>{
-    const user = this.usersRepository.create(createUserDto);
+  /*
+   * 회원 가입
+   */
+  async signUp(createUserDto: CreateUserDto): Promise<void>{
+    if (await this.isEmailExist(createUserDto.email)) throw new ConflictException(`There is an account with that email already.`);
+    if (await  this.isUserNameExist(createUserDto.user_name)) throw new ConflictException(`There is an account with that username already.`);
+    const user:User = this.usersRepository.create({
+      user_email: createUserDto.email,
+      user_name: createUserDto.user_name
+    });
     await this.usersRepository.save(user);
   }
 
-  async findAll(): Promise<User[]> {
-    return await this.usersRepository.find();
+  /*
+   * 이메일 존재 유무 체크
+   */
+  async isEmailExist(email: string): Promise<boolean>{
+    const isExist: number = await this.usersRepository.count({where: {user_email: email}})
+    return isExist > 0;
+  }
+
+  /*
+   * 유저이름 존재 유무 체크
+   */
+  async isUserNameExist(user_name: string): Promise<boolean>{
+    const isExist: number = await this.usersRepository.count({where:{user_name: user_name}});
+    return isExist > 0;
+  }
+
+  /*
+   * 회원 가입 이메일 보내기
+   */
+  async SendSignUpConfirmEmail(createUserDto: CreateUserDto){
+    return '';
+  }
+
+  /*
+   * 로그인 URL 링크 이메일 전송
+   */
+  async emailVerify(emailVerifyDto: EmailVerifyDto){
+    if (await this.isEmailExist(emailVerifyDto.email)){
+      console.log(emailVerifyDto.email, '가 있습니다')
+    }
   }
 }
