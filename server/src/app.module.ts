fb526d399ee9932e853e5c5e52758fb4b1b20fb0
beenchangseo <beenchangseo@gmail.com>
2022-07-13 18:15:42 +0900

    [백엔드] @nestjs/mailer & 이메일 모듈 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,16 +1,40 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
+import {MailerModule} from "@nestjs-modules/mailer";
+import {EjsAdapter} from "@nestjs-modules/mailer/dist/adapters/ejs.adapter";
+import { EmailModule } from './email/email.module';
 
 @Module({
   imports: [
     UserModule,
     DatabaseModule,
+    MailerModule.forRootAsync({
+      useFactory: () => ({
+        transport: {
+          host: 'smtp.naver.com',
+          port: 587,
+          auth: {
+            user: 'seochangbin_1',
+            pass: '18tkfkdgo'
+          },
+        },
+        defaults: {
+          from: 'seochangbin_1@naver.com',
+        },
+        template: {
+          dir: __dirname + '/templates',
+          adapter: new EjsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
+    EmailModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {
 }
