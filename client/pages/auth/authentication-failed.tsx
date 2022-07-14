b0827be2c6d5f772f5b2e008506d19004481c8c4
beenchangseo <beenchangseo@gmail.com>
2022-07-14 23:09:20 +0900

    [프론트엔드] 로그인 실패 페이지

--- client/pages/auth/authentication-failed.tsx
+++ client/pages/auth/authentication-failed.tsx
@@ -1,11 +1,27 @@
 const AuthenticationFailed = ()=> {
 
     return<>
-        <h1>Could not log you in.</h1>
-        <h3>반드시 같은 기기, 같은 브라우저에서 로그인 링크를 요청하고, 클릭해주세요.</h3>
-        <h3>
-            <a href={'/auth/login'}>Try again</a>
-        </h3>
+        <div className={'fail-container'}>
+            <h1>Could not log you in.</h1>
+            <h3>반드시 같은 기기, 같은 브라우저에서 로그인 링크를 요청하고, 클릭해주세요.</h3>
+            <h3>
+                <a href={'/auth/login'}>Try again</a>
+            </h3>
+        </div>
+        <style jsx>{`
+          .fail-container{
+            color: white;
+            display: flex;
+            flex-flow: column;
+            align-items: center;
+            height: 100vh;
+            width: 100vw;
+            justify-content: center;
+          }
+          .fail-container > h3 > a {
+            color: white;
+          }
+        `}</style>
     </>
 }
 export default AuthenticationFailed
