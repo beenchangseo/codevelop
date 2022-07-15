3334cb5332340c3fd48580c20a92cb83b9c226a1
beenchangseo <beenchangseo@gmail.com>
2022-07-15 23:10:33 +0900

    [프론트엔드] getServerSideProps에서 JWT토큰으로 인증 요청

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,25 +1,45 @@
 import type {GetServerSideProps, NextPage} from 'next'
-import cookies from 'next-cookies'
-import ChallengeNav from "../components/learn/ChallengeNav";
-import ChallengeContents from "../components/learn/ChallengeContents";
-import TopNav from "../components/TopNav";
+import axios from "axios";
+import cookies from "next-cookies";
+import {useEffect} from "react";
 
-const Home: NextPage = () => {
+interface HomePageProps{
+  user_name: string;
+}
+
+const Home: NextPage<HomePageProps> = (props: HomePageProps) => {
+  useEffect(()=>{
+    console.log(props.user_name);
+  },[])
   return <>
-    <TopNav/>
-    <div className={"learn-contents"}>
-      <ChallengeNav/>
-      <ChallengeContents/>
-    </div>
+    <div style={{color: "white"}}>Home Page</div>
   </>
 }
 
 export default Home
 export const getServerSideProps:GetServerSideProps = async (context) => { // SSR
-  const token = cookies(context).jwt;
-  console.log(token)
-
-  return {
-    props: {}
+  const token: string | undefined = cookies(context).jwt;
+  if (!token){
+    return {
+      props: {
+        user_name: null
+      }
+    }
+  }else{
+    const response = await axios.get('http://localhost:3001/user/token-verify',{
+      headers: {
+        'Cache-Control': 'no-cache',
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    })
+        .then((response) => {return response.data});
+    console.log('index SSR ',response.user_name)
+    return {
+      props: {
+        user_name: response.user_name,
+      }
+    }
   }
+
 }
