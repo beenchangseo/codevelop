ca6470483937eeba14f3f47aa5b7582d50bd8b7f
beenchangseo <beenchangseo@gmail.com>
2022-07-15 23:17:11 +0900

    [백엔드] 중복 함수 제거 및 이메일토큰 로직 수정

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -2,11 +2,13 @@ import {ConflictException, Inject, Injectable} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import {User} from "./entities/user.entity";
-import {Repository} from "typeorm";
+import {FindOneOptions, Repository} from "typeorm";
 import {EmailVerifyDto} from "./dto/email-verify.dto";
 import {JwtService} from "@nestjs/jwt";
 import {Payload} from "./security/payload.interface";
 import {EmailService} from "../email/email.service";
+import { randomBytes } from 'crypto'
+import {FindManyOptions} from "typeorm/find-options/FindManyOptions";
 
 @Injectable()
 export class UserService {
@@ -17,13 +19,19 @@ export class UserService {
       private readonly emailService: EmailService,
   ) {
   }
+  /*
+   * Find one User by FindOneOptions<User>
+   */
+  async findByFields(option: FindOneOptions<User>): Promise<User | undefined>{
+    return await this.usersRepository.findOne(option);
+  }
 
   /*
    * 회원 가입
    */
   async signUp(createUserDto: CreateUserDto): Promise<void>{
-    if (await this.isEmailExist(createUserDto.email)) throw new ConflictException(`There is an account with that email already.`);
-    if (await  this.isUserNameExist(createUserDto.user_name)) throw new ConflictException(`There is an account with that username already.`);
+    await this.isOptionExist({where: {user_email: createUserDto.email}});
+    await this.isOptionExist({where: {user_name: createUserDto.user_name}});
     const user:User = this.usersRepository.create({
       user_email: createUserDto.email,
       user_name: createUserDto.user_name
@@ -32,64 +40,49 @@ export class UserService {
   }
 
   /*
-   * 이메일 존재 유무 체크
+   * 중복 필드 존재 유무 체크
    */
-  async isEmailExist(email: string): Promise<boolean>{
-    const isExist: number = await this.usersRepository.count({where: {user_email: email}})
-    return isExist > 0;
-  }
-
-  /*
-   * 유저이름 존재 유무 체크
-   */
-  async isUserNameExist(user_name: string): Promise<boolean>{
-    const isExist: number = await this.usersRepository.count({where:{user_name: user_name}});
-    return isExist > 0;
-  }
-
-  /*
-   * Find one user entity by Email
-   */
-  async findOneUserByEmail(email: string): Promise<User | undefined>{
-    return await this.usersRepository.findOne({where: {user_email: email}});
-  }
-
-  /*
-   * 회원 가입 이메일 보내기
-   */
-  async SendSignUpConfirmEmail(createUserDto: CreateUserDto){
-    return '';
+  async isOptionExist(option: FindManyOptions<User>): Promise<void>{
+    const cnt: number = await this.usersRepository.count(option);
+    const {user_email, user_name}: any = option.where
+    if (cnt !== 0) throw new ConflictException(
+        `There is an account with that 
+                    ${user_email === undefined ? '':user_email} 
+                    ${user_name === undefined ? '':user_name} already.`
+    );
   }
 
   /*
    * 로그인 URL 링크 이메일 전송
    */
   async emailVerify(emailVerifyDto: EmailVerifyDto){
-    const userFind = await this.findOneUserByEmail(emailVerifyDto.email);
+    const userFind: User = await this.findByFields({where:{user_email: emailVerifyDto.email}});
     if (userFind){
-      const payload: Payload = { user_name: userFind.user_name, user_email: userFind.user_email };
-      const accessToken = this.jwtService.sign(payload);
-      const loginUrl = `http://localhost:3000/auth/confirm?signupVerifyToken=${accessToken}`;
-      await this.emailService.signInMail(userFind, loginUrl);
-      //
-      // return {
-      //   accessToken: accessToken
-      // }
+      const loginToken = randomBytes(16).toString('hex');
+      await this.userLoginTokenUpdate(userFind, loginToken);
+      const loginUrl = `http://localhost:3000/auth/confirm?signInVerifyToken=${loginToken}`;
+      await this.emailService.SendSignInMail(userFind, loginUrl);
     }else throw new ConflictException('This user does not exist.');
   }
 
   /*
-   * jwt token validate
+   * 유저 로그인 토큰 업데이트
    */
-  async tokenValidateUser(payload: Payload): Promise<User | undefined>{
-    return await this.findOneUserByEmail(payload.user_email);
+  async userLoginTokenUpdate(findUser: User, loginToken: string): Promise<void>{
+    findUser.user_login_token = loginToken;
+    await this.usersRepository.save(findUser);
   }
 
   /*
-   * get Cookie With JwtToken
+   * get User With Payload
    */
-  getCookieWithJwtToken(user: User): string{
-    const payload: Payload = {user_name: user.user_name, user_email: user.user_email};
+  async getUserWithPayload(payload: Payload): Promise<User | undefined>{
+    return await this.findByFields({where: {user_email: payload.user_email}});
+  }
+
+  async getJwtByUserLoginToken(token: string): Promise<string>{
+    const user: User | undefined = await this.findByFields({where: {user_login_token: token}});
+    const payload: Payload = { user_name: user.user_name, user_email: user.user_email };
     return this.jwtService.sign(payload);
   }
 }
