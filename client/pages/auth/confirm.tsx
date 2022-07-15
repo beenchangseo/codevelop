fcc217e74452b4f6ab8f2df3e5936224eb5bb296
beenchangseo <beenchangseo@gmail.com>
2022-07-15 23:09:03 +0900

    [프론트엔드] confirm 이메일토큰으로 JWT토큰 받기

--- client/pages/auth/confirm.tsx
+++ client/pages/auth/confirm.tsx
@@ -4,26 +4,24 @@ import {useEffect} from "react";
 
 const Confirm = () => {
     const router = useRouter();
-
     useEffect(() => {
         if (router.isReady) {
-            const { signupVerifyToken } = router.query
-            const params = { signupVerifyToken:signupVerifyToken };
+            const { signInVerifyToken } = router.query
+            const params = { signInVerifyToken:signInVerifyToken };
             axios.get('/api/user/confirm',{params})
                 .then(response => {
                     if (response.status === 200){
-                        router.push('/');
+                        router.replace('/')
                     }
                 }).catch(error => {
                     if (error.response.status === 401){
-                        router.push('/auth/authentication-failed')
+                         router.replace('/auth/authentication-failed')
                     }
                 });
         }
     }, [router.isReady]);
 
-    return<>
-        <div>Confirm Page</div>
+    return <>
     </>
 }
 export default Confirm
