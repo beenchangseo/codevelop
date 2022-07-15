56a45a920cb50c2b9fc4831210f354c4b08e7b6e
beenchangseo <beenchangseo@gmail.com>
2022-07-15 23:08:14 +0900

    [프론트엔드] confirm component 는 Layout 제외

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,16 +2,18 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from "../components/Layout";
 import Head from "next/head";
+import {Fragment} from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-
+function MyApp({ Component, pageProps, ...appProps }: AppProps) {
+  const isLayoutNeeded = appProps.router.pathname.includes(`/confirm`);
+  const LayoutComponent = !isLayoutNeeded ? Layout : Fragment;
   return <>
     <Head>
       <title>코테 연습장</title>
     </Head>
-    <Layout>
+    <LayoutComponent>
       <Component {...pageProps} />
-    </Layout>
+    </LayoutComponent>
   </>
 }
 
