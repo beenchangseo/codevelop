a073319f4ca75cc4bb125b4ef87122860ebcd255
beenchangseo <beenchangseo@gmail.com>
2022-07-15 23:13:32 +0900

    [프론트엔드] 인증된 유저 props 처리 및 로그아웃 처리

--- client/components/TopNav.tsx
+++ client/components/TopNav.tsx
@@ -1,6 +1,20 @@
 import styles from "../styles/components/topNavigation.module.css"
 
-const TopNav = () => {
+import {useState} from "react";
+import {deleteCookie} from "cookies-next";
+import {useRouter} from "next/router";
+
+interface pageProps{
+    user_name: string;
+}
+
+const TopNav = (props: pageProps) => {
+    const [userName] = useState<string>(props.user_name);
+    const router = useRouter()
+    const logoutHandler = () => {
+        deleteCookie('jwt');
+        router.reload();
+    };
     return (
         <>
             <div className={styles.topNavContainer}>
@@ -9,20 +23,33 @@ const TopNav = () => {
                 </a>
                 <ol className={styles.breadcrumb}>
                     <li>
-                        <a href={'/'}>코딩테스트 연습</a>
+                        <a href={'/'}>CODEVELOPE</a>
                     </li>
-                    <li>
-                        <a href={'/'}>문제 이름</a>
-                    </li>
+                    {/*<li>*/}
+                    {/*    <a href={'/'}>문제 이름</a>*/}
+                    {/*</li>*/}
                 </ol>
                 <div className={styles.navbarRight}>
                     <ul className={styles.navbarRightUl}>
-                        <li>
-                            <a href={'/'}>도움말</a>
-                        </li>
-                        <li>
-                            <a href={'/auth/login'}>로그인</a>
-                        </li>
+                        {userName ?
+                            <>
+                                <li>
+                                    <a href={'/'}>{userName}</a>
+                                </li>
+                                <li>
+                                    <a onClick={logoutHandler}>Logout</a>
+                                </li>
+                            </>
+                             :
+                            <>
+                                <li>
+                                    <a href={'/auth/login'}>Login</a>
+                                </li>
+                                <li>
+                                    <a href={'/auth/signup'}>Join</a>
+                                </li>
+                            </>
+                        }
                     </ul>
                 </div>
             </div>
