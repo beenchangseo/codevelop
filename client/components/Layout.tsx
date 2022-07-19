92cfb8d7e372da11c11562402671364f1539e04b
beenchangseo <beenchangseo@gmail.com>
2022-07-19 18:24:33 +0900

    [프론트엔드] 상단바 Nav 고정

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,5 +1,6 @@
 import {ReactElement} from "react";
 import TopNav from "./TopNav";
+import Footer from "./Footer";
 
 
 type LayoutProps = {
@@ -10,9 +11,20 @@ const Layout = ({children}: LayoutProps) => {
 
     return(
         <>
-            <TopNav user_name={children.props.user_name}/>
+            <div className={'nav'} style={{
+                position: "sticky",
+                top: 0,
+                backgroundColor: "#0c151c",
+                justifyContent: "center",
+                display: "flex"
+            }}>
+                <TopNav user_name={children.props.user_name}/>
+            </div>
             <div className={'main'} style={{backgroundColor: "#263747"}}>
                 {children}
+            </div>
+            <div className={'footer'} style={{backgroundColor: "#263747"}}>
+                <Footer/>
             </div>
         </>
     )
