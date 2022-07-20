919505f1a95bba7bea99fbf0995e46354ccddcf7
beenchangseo <beenchangseo@gmail.com>
2022-07-20 23:34:23 +0900

    [프론트엔드] 메인 페이지 수정

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,23 +1,74 @@
 import type {GetServerSideProps, NextPage} from 'next'
 import axios from "axios";
 import cookies from "next-cookies";
-import {useEffect} from "react";
+import {useState} from "react";
+import Link from "next/link";
 
 interface HomePageProps{
   user_name: string;
 }
 
 const Home: NextPage<HomePageProps> = (props: HomePageProps) => {
-  useEffect(()=>{
-    console.log(props.user_name);
-  },[])
+  const [loginUser] = useState<string>(props.user_name);
+
   return <>
-    <div style={{color: "white"}}>Home Page</div>
+    <div className={'index-container'}>
+      <div className={'box'}>
+        <h1>
+          Coding And Develope.
+          <br/>
+          <span>Learn to code.</span>
+        </h1>
+      </div>
+      <div className={'box'}>
+        <p style={{
+          fontSize: '17px'
+        }}>
+          코딩은 진짜를 만들어보는거야!<br/>
+          웹 컴파일러에서 쉽게 따라 만들면서 코딩을 배우세요.
+        </p>
+      </div>
+      <div className={'box'}>
+        {loginUser === null ?
+            <button className={'btn'}><Link href={'/learn/demo'}><a>Go to start →</a></Link></button>
+            : <button className={'btn'}><Link href={'/learn/demo'}><a>Go to learn →</a></Link></button>}
+      </div>
+      <div className={'box'}>
+        <img src={'/icons/programming.gif'} width={400} height={300} style={{marginBottom: 30}}/>
+      </div>
+    </div>
+    <style jsx>{`
+      .index-container {
+        color: white;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
+      .box{
+        text-align: center;
+      }
+      .btn{
+        border: 1px solid white;
+        border-radius: 7px;
+        padding: 10px;
+        cursor: pointer;
+        font-family: 'Noto Sans KR', sans-serif;
+        font-size: 16px;
+        margin: 20px;
+      }
+      h1{
+        margin: 0;
+        padding-top: 4rem;
+        font-size: 50px;
+        font-style: oblique;
+      }
+    `}</style>
   </>
 }
 
 export default Home
-export const getServerSideProps:GetServerSideProps = async (context) => { // SSR
+export const getServerSideProps:GetServerSideProps = async (context) => {
   const token: string | undefined = cookies(context).jwt;
   if (!token){
     return {
@@ -32,9 +83,7 @@ export const getServerSideProps:GetServerSideProps = async (context) => { // SSR
         'Authorization': `Bearer ${token}`,
         'Content-Type': 'application/json'
       }
-    })
-        .then((response) => {return response.data});
-    console.log('index SSR ',response.user_name)
+    }).then((response) => {return response.data});
     return {
       props: {
         user_name: response.user_name,
