31371d8ef755b3d360bb6eeaf2e557f80e9f362d
beenchangseo <beenchangseo@gmail.com>
2022-07-22 19:00:26 +0900

    [프론트엔드] 상단네비게이션 전체 수정

--- client/components/TopNav.tsx
+++ client/components/TopNav.tsx
@@ -3,8 +3,9 @@ import styles from "../styles/components/topNavigation.module.css"
 import {useState} from "react";
 import {deleteCookie} from "cookies-next";
 import {useRouter} from "next/router";
+import Link from "next/link";
 
-interface pageProps{
+interface pageProps {
     user_name: string;
 }
 
@@ -17,42 +18,124 @@ const TopNav = (props: pageProps) => {
     };
     return (
         <>
-            <div className={styles.topNavContainer}>
-                <a className={styles.navbarLogo} href={'/'} >
-                    <img src={'/logo.png'} alt={"logo"}/>
-                </a>
-                <ol className={styles.breadcrumb}>
-                    <li>
-                        <a href={'/'}>CODEVELOPE</a>
-                    </li>
-                    {/*<li>*/}
-                    {/*    <a href={'/'}>문제 이름</a>*/}
-                    {/*</li>*/}
-                </ol>
-                <div className={styles.navbarRight}>
-                    <ul className={styles.navbarRightUl}>
-                        {userName ?
-                            <>
-                                <li>
-                                    <a href={'/'}>{userName}</a>
-                                </li>
-                                <li>
-                                    <a onClick={logoutHandler}>Logout</a>
-                                </li>
-                            </>
-                             :
-                            <>
-                                <li>
-                                    <a href={'/auth/login'}>Login</a>
-                                </li>
-                                <li>
-                                    <a href={'/auth/signup'}>Join</a>
-                                </li>
-                            </>
-                        }
-                    </ul>
+            <div className={'nav-container'}>
+                <div className={'logo-section'}>
+                    <Link href={'/'}>
+                        <img src={'/logo.svg'} alt={"logo"}/>
+                    </Link>
+                </div>
+                <div className={'menu-section'}>
+                    <Link href={'/problem'}>
+                        <a className={'menu-items'}>Problems</a>
+                    </Link>
+                    <Link href={'/learn/demo'}>
+                        <a className={'menu-items'}>Challenges</a>
+                    </Link>
+                    <Link href={'/community'}>
+                        <a className={'menu-items'}>Community</a>
+                    </Link>
+                </div>
+                <div className={'user-section'}>
+                    <Link href={'/auth/login'}>
+                        <a className={'menu-items'}>Login</a>
+                    </Link>
+                    <Link href={'/auth/signup'}>
+                        <a className={'menu-items'}>Join</a>
+                    </Link>
                 </div>
             </div>
+
+            <style jsx>{`
+              .nav-container {
+                color: white;
+                padding-left: 2rem;
+                padding-right: 2rem;
+                display: flex;
+                align-items: center;
+                max-width: 80rem;
+                margin-left: auto;
+                margin-right: auto;
+              }
+              .logo-section{
+                padding: 4px;
+                cursor: pointer;
+              }
+              .logo-section > img {
+                width: 41px;
+                height: 32px;
+                vertical-align: middle;
+                margin: 5px 5px 5px 0;
+              }
+
+              .menu-section {
+                display: flex;
+              }
+
+              .menu-items {
+                margin-left: 1.5rem;
+                cursor: pointer;
+                color: #909090;
+              }
+
+              .menu-items:hover {
+                color: white;
+              }
+
+              .user-section {
+                display: flex;
+                flex-grow: 1;
+                justify-content: end;
+              }
+            `}</style>
+
+
+            {/*<div className={styles.topNavContainer}>*/}
+            {/*    <a className={styles.navbarLogo} href={'/'} >*/}
+            {/*        <img src={'/logo.png'} alt={"logo"}/>*/}
+            {/*    </a>*/}
+            {/*    <ol className={styles.breadcrumb}>*/}
+            {/*        <li>*/}
+            {/*            <a href={'/'}>CODEVELOPE</a>*/}
+            {/*        </li>*/}
+            {/*        <li>*/}
+            {/*            <a href={'/problem'}>문제 연습</a>*/}
+            {/*        </li>*/}
+            {/*        <li>*/}
+            {/*            <a href={'/problem'}>문제 만들기</a>*/}
+            {/*        </li>*/}
+            {/*        <li>*/}
+            {/*            <a href={'/problem'}>커뮤니티</a>*/}
+            {/*        </li>*/}
+            {/*    </ol>*/}
+            {/*    <div className={styles.navbarRight}>*/}
+            {/*        <ul className={styles.navbarRightUl}>*/}
+            {/*            {userName ?*/}
+            {/*                <>*/}
+            {/*                    <li>*/}
+            {/*                        <a href={'/'} style={{fontSize: '13px'}}>*/}
+            {/*                            <span>*/}
+            {/*                                {userName}*/}
+            {/*                            </span>*/}
+            {/*                            님*/}
+            {/*                        </a>*/}
+            {/*                    </li>*/}
+            {/*                    <li>*/}
+            {/*                        <a onClick={logoutHandler}>Logout</a>*/}
+            {/*                    </li>*/}
+            {/*                </>*/}
+            {/*                 :*/}
+            {/*                <>*/}
+            {/*                    <li>*/}
+            {/*                        <a href={'/auth/login'}>Login</a>*/}
+            {/*                    </li>*/}
+            {/*                    <li>*/}
+            {/*                        <a href={'/auth/signup'}>Join</a>*/}
+            {/*                    </li>*/}
+            {/*                </>*/}
+            {/*            }*/}
+            {/*        </ul>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
         </>
     )
 }
